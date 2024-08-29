c8a032d5d6aa99ca27d16c4d44465ac0f6e0104e
J-Barta <53406552+J-Barta@users.noreply.github.com>
2024-08-29 14:17:52 -0400

    prompt users when they have no installed mods

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mosim_modloader/mod-displays/mod_list_view.dart';
+import 'package:mosim_modloader/util/constants.dart';
 import 'package:mosim_modloader/util/mod.dart';
 
 class Home extends StatefulWidget {
   final List<Mod> installedMods;
   final Function onInstallsChanged;
 
-  const Home({super.key, required this.installedMods, required this.onInstallsChanged});
+  const Home(
+      {super.key,
+      required this.installedMods,
+      required this.onInstallsChanged});
 
   @override
   State<Home> createState() => _HomeState();
@@ -19,9 +23,26 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Expanded(
-            child: ListView(
-              children: widget.installedMods.map((e) => ModListView(mod: e, installed: true, onInstallsChanged: widget.onInstallsChanged,)).toList(),
-            ),
+            child: widget.installedMods.isNotEmpty
+                ? ListView(
+                    children: widget.installedMods
+                        .map((e) => ModListView(
+                              mod: e,
+                              installed: true,
+                              onInstallsChanged: widget.onInstallsChanged,
+                            ))
+                        .toList(),
+                  )
+                : Center(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: 500),
+                      child: Text(
+                          "Thanks for installing Reactor! It looks like you have no mods installed yet. Navigate to the Mods tab in the hamburger menu to get playing!",
+                          style: StyleConstants.h3Style,
+                          textAlign: TextAlign.center,
+                          ),
+                    ),
+                  ),
           )
         ],
       ),
