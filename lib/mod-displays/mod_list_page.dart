659b574ad259016ba768a4c588cce6f7a6725f22
J-Barta <53406552+J-Barta@users.noreply.github.com>
2024-09-04 12:44:07 -0400

    add filter for installed vs. uninstalled mods

--- lib/mod-displays/mod_list_page.dart
+++ lib/mod-displays/mod_list_page.dart
@@ -28,6 +28,7 @@ class _ModListPageState extends State<ModListPage> {
   TextEditingController searchController = TextEditingController();
 
   bool showUnverified = false;
+  bool includeInstalled = false;
 
   String sortCondition = "downloads";
   bool sortOrder = true;
@@ -58,8 +59,6 @@ class _ModListPageState extends State<ModListPage> {
   }
 
   List<Mod> applySearch(List<Mod> mods) {
-
-
     if (searchController.text == "") {
       return mods;
     }
@@ -88,6 +87,12 @@ class _ModListPageState extends State<ModListPage> {
           modsToDisplay.where((element) => element.verified).toList();
     }
 
+    if (!includeInstalled) {
+      modsToDisplay = modsToDisplay
+          .where((element) => !widget.installedMods.contains(element))
+          .toList();
+    }
+
     modsToDisplay = applySearch(modsToDisplay);
 
     return Scaffold(
@@ -112,6 +117,17 @@ class _ModListPageState extends State<ModListPage> {
                         hintText: "Search", border: OutlineInputBorder()),
                   ),
                 ),
+              ),
+              Checkbox(
+                  value: includeInstalled,
+                  onChanged: (value) {
+                    setState(() {
+                      includeInstalled = value!;
+                    });
+                  }),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Include Installed"),
               ),
               Checkbox(
                   value: showUnverified,
@@ -159,18 +175,30 @@ class _ModListPageState extends State<ModListPage> {
                   )),
             ],
           ),
-          Expanded(
-            child: ListView(
-              children: modsToDisplay
-                  .map((e) => ModListView(
-                      user: widget.user,
-                      mod: e,
-                      installed: widget.installedMods.contains(e),
-                      onInstallsChanged: widget.onInstallsChanged,
-                      reloadModList: widget.reloadModList))
-                  .toList(),
-            ),
-          )
+          modsToDisplay.isNotEmpty
+              ? Expanded(
+                  child: ListView(
+                    children: modsToDisplay
+                        .map((e) => ModListView(
+                            user: widget.user,
+                            mod: e,
+                            installed: widget.installedMods.contains(e),
+                            onInstallsChanged: widget.onInstallsChanged,
+                            reloadModList: widget.reloadModList))
+                        .toList(),
+                  ),
+                )
+              : Expanded(
+                  child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "No mods meet your search criteria! Change your search settings to find new mods!",
+                      style: StyleConstants.titleStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ))
         ],
       ),
     );
